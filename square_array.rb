6d4b1e05fcda7bb03e8bc989e744bece4f2677aa
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-22 23:41:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -6,6 +6,6 @@ def square_array(array)
   return arr
 end
 
-def square_array2(array)
- return array.collect { |num| num ** 2 }
-end
+# def square_array2(array)
+#  return array.collect { |num| num ** 2 }
+# end
